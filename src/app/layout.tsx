a760b266509a8a81841c469dbc43caa0f0728249
Mehedi Hasan <career.mehedihasan@gmail.com>
2025-07-15 04:24:55 +0000

    So listen carefully. You have my agency logo link. So please replace the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
   title: 'Stova Media - Powering Digital Growth with Ads, AI & Web',
   description: 'We are a results-obsessed digital agency helping businesses grow with performance ads, powerful websites, and smart AI agents.',
   keywords: ['Meta Ads', 'Website Development', 'AI Agents', 'Digital Marketing', 'Stova Media'],
+  icons: {
+    icon: 'https://scontent.fixb1-1.fna.fbcdn.net/v/t39.30808-6/496951606_122118032312829021_4712571896223912843_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=vbRkCDnXTeIQ7kNvwFIACiI&_nc_oc=AdkbLaswwvzf7kWPsDJiLQp8B5cbo8KrdSGTR8pRTkHuhAbfe0HphBuM5bfGrzR74Y4&_nc_zt=23&_nc_ht=scontent.fixb1-1.fna&_nc_gid=nYZLC8s-yS5Ch34G4rueSA&oh=00_AfQUK48s2TbPKtF00GUyTi1PJ2v98BJyfK8sGwul_lG53A&oe=687B139C',
+  },
 };
 
 export default function RootLayout({
